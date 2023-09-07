42051755d9c0fd841f6695234520a6c354e27790
senthamizh-cse <senthamizh676@gmail.com>
2023-09-07 14:54:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Fetchcomponent from './components/FetchComponent/Fetchcomponent'
+import'./App.css'
+function App() {
+  return (
+    <React.Fragment>
+      <Fetchcomponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/components/FetchComponent/Fetchcomponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fetchcomponent = () => {
+  return (
+    <div>Fetchcomponent</div>
+  )
+}
+
+export default Fetchcomponent
